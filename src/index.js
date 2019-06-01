4ddf3bd3b53c653fc3ecd605870d8b870e07898d
Patrick Goulding <patrickgoulding@gmail.com>
2019-06-01 15:21:40 -0600

    main data appending to DOM

--- src/index.js
+++ src/index.js
@@ -4,22 +4,22 @@ const data = {};
 
 fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1903/users/users')
 .then(response => response.json())
-.then(users => data.users = users.data)
+.then(remote => data.users = remote.users)
 .catch(err => console.error(err))
 
 fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1903/rooms/rooms')
 .then(response => response.json())
-.then(rooms => data.rooms = rooms.data)
+.then(remote => data.rooms = remote.rooms)
 .catch(err => console.error(err))
 
 fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1903/bookings/bookings')
 .then(response => response.json())
-.then(bookings => data.bookings = bookings.data)
+.then(remote => data.bookings = remote.bookings)
 .catch(err => console.error(err))
 
 fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1903/room-services/roomServices')
 .then(response => response.json())
-.then(services => data.services = services.data)
+.then(remote => data.services = remote.roomServices)
 .catch(err => console.error(err))
 
 const todaysDate = () => {
@@ -47,11 +47,11 @@ import $ from 'jquery';
 import './css/base.scss';
 
 import Main from '../src/Main';
-import UserRepo
-import Customer from '../src/Customer';
-import RoomService from '../src/RoomService';
-import Room from '../src/Room';
-import BookingsRepo from '../src/BookingsRepo';
+// import UserRepo
+// import Customer from '../src/Customer';
+// import RoomService from '../src/RoomService';
+// import Room from '../src/Room';
+// import BookingsRepo from '../src/BookingsRepo';
 // import RoomRepo from '../src/RoomRepo'
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
@@ -61,14 +61,29 @@ import './images/turing-logo.png'
 $(document).ready(function () {
   
   console.log(todaysDate())
+  $('ul.tabs li').click(function () {
+    var tab_id = $(this).attr('data-tab');
 
-  const main = new Main(data.)
-  const allUsers = new UserRepo(data.users)
-  const allRooms = new RoomRepo(data.rooms)
-  const allBookings = new BookingsRepo(data.bookings)
-  const allServices = new RoomServiceRepo(data.roomservice)
+    $('ul.tabs li').removeClass('current');
+    $('.tab-content').removeClass('current');
+
+    $(this).addClass('current');
+    $("#" + tab_id).addClass('current');
+  })
+  
+  setTimeout(() => {
+    const main = new Main(data.bookings, data.services, data.rooms, '21/08/2019')
+  }, 3000);
+
+  // const allUsers = new UserRepo(data.users)
+  // const allRooms = new RoomRepo(data.rooms)
+  // const allBookings = new BookingsRepo(data.bookings)
+  // const allServices = new RoomServiceRepo(data.roomservice)
 })
-
+$('#tab__customers-search').on('input', function () {
+  let typed = $('tab__customers-search').val()
+  
+})
 // $('#btn_customer-select').on('click', function () {
 //   let user = $('input__user-select').val()
 //   const customer = new Customer(usersData, user)
