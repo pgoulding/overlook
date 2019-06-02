46b7c366b7b5fee1900d545bb18e1d88f698583a
Patrick Goulding <patrickgoulding@gmail.com>
2019-06-01 20:45:42 -0600

    add domupdates and chai spies

--- src/RoomService.js
+++ src/RoomService.js
@@ -1,32 +1,39 @@
+import domUpdates from "./domUpdates";
+
 class RoomService {
   constructor(data, customer) {
     this.data = data;
     this.customer = customer;
   }
-
+  
+  // - Breakdown of dates and dollar amounts for room service
   breakDown() {
     let customersService = [...this.data].filter(service => service.userID === this.customer)
     if(customersService.length === 0) {
       return 'No Information to display'
     } else {
+      domUpdates.ordersBreakDown(customersService)
       return customersService
     }
   }
-
+  
+  // - Total dollar amount spent on room service for a particular day
   perDate(date) {
     let dayCost = [...this.data].find(food => food.date === date).totalCost
     if (!dayCost) {
       return 'No Information to display' 
     } else {
+      
       return dayCost;
     }
   }
-
+// - Total dollar amount spent on room service for a all days ever
   forAllDays() {
     let totalCost = this.breakDown().reduce((acc, item) => acc += item.totalCost, 0)
     if (!totalCost) {
       return 'No Information to Display'
     } else {
+      domUpdates.totalOrdersCost()
       return totalCost;
     }
   }
@@ -39,9 +46,7 @@ export default RoomService;
 //  clicking on the`Orders` icon  will take me to the`Orders` tab.
 //  The`Orders` tab will display customer specific information:
 
-// - Breakdown of dates and dollar amounts for room service
-// - Total dollar amount spent on room service for a particular day
-// - Total dollar amount spent on room service for a all days ever
+
 
 
 //  If there is no valid information for this customer,
