08bf4df75434ec23bb8499fbb2d1b260a4fbe6e3
Patrick Goulding <patrickgoulding@gmail.com>
2019-06-02 09:06:49 -0600

    bookingRepo instantiated

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
+/* eslint-disable max-len */
+
 const data = {};
 
 fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1903/users/users')
-.then(response => response.json())
-.then(remote => data.users = remote.users)
-.catch(err => console.error(err))
+  .then(response => response.json())
+  .then(remote => data.users = remote.users)
+  .catch(err => console.error(err))
 
 fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1903/rooms/rooms')
-.then(response => response.json())
-.then(remote => data.rooms = remote.rooms)
-.catch(err => console.error(err))
+  .then(response => response.json())
+  .then(remote => data.rooms = remote.rooms)
+  .catch(err => console.error(err))
 
 fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1903/bookings/bookings')
-.then(response => response.json())
-.then(remote => data.bookings = remote.bookings)
-.catch(err => console.error(err))
+  .then(response => response.json())
+  .then(remote => data.bookings = remote.bookings)
+  .catch(err => console.error(err))
 
 fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1903/room-services/roomServices')
-.then(response => response.json())
-.then(remote => data.services = remote.roomServices)
-.catch(err => console.error(err))
+  .then(response => response.json())
+  .then(remote => data.services = remote.roomServices)
+  .catch(err => console.error(err))
 
 const todaysDate = () => {
   let today = new Date();
@@ -59,11 +59,13 @@ import RoomRepo from '../src/RoomRepo'
 import './images/turing-logo.png'
 import domUpdates from './domUpdates';
 import RoomServiceRepo from './RoomServiceRepo';
+import BookingRepo from './BookingsRepo';
 
 let userRepo;
 let main; 
 let orderRepo;
-// let roomRepo
+let bookingRepo;
+// let roomRepo;
 $(document).ready(function () {
   
   console.log(todaysDate())
@@ -81,6 +83,7 @@ $(document).ready(function () {
     main = new Main(data.bookings, data.services, data.rooms, '15/07/2019')
     userRepo = new UserRepo(data.users)
     orderRepo = new RoomServiceRepo(data.services, '15/07/2019')
+    bookingRepo = new BookingRepo(data.bookings)
   }, 3000);
 
   // const allUsers = new UserRepo(data.users)
