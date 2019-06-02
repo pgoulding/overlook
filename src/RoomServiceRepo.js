282a2e01e514e764acef101a236f7694261106e4
Patrick Goulding <patrickgoulding@gmail.com>
2019-06-01 21:50:49 -0600

    total for all room service per day tested and appending

--- src/RoomServiceRepo.js
+++ src/RoomServiceRepo.js
@@ -4,14 +4,17 @@ class RoomServiceRepo {
   constructor(data, date) {
     this.data = data;
     this.date = date;
-    this.services = this.totalServicesDate(this.date)
+    this.services = this.totalServicesDate(date)
   }
 
   totalServicesDate(date) {
+    console.log('what up')
     let todaysServices = this.data.filter(day => day.date === date)
     domUpdates.allRoomServiceOnDate(todaysServices)
+    console.log(todaysServices)
     return todaysServices
   }
+
 }
 
 export default RoomServiceRepo;
--- test/RoomServiceRepo-test.js
+++ test/RoomServiceRepo-test.js
@@ -18,4 +18,18 @@ describe('Room Service Repo', function () {
     expect(RoomServiceRepo).to.be.a('function')
   })
 
+  it('should be an instantiation of RoomServiceRepo', function () {
+    expect(roomServiceRepo).to.be.an.instanceOf(RoomServiceRepo)
+  })
+
+  it('should take in the Room Service data, and a date', function () {
+    expect(roomServiceRepo.data).to.be.equal(roomServices)
+    expect(roomServiceRepo.date).to.equal('21/08/2019')
+  })
+
+  it('should display all the service charges for the supplied date', function () {
+    expect(roomServiceRepo.services).to.be.an('array').and.have.length(3)
+    // console.log(roomServiceRepo.services)
+  })
+
 })
