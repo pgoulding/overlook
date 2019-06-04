94e157df4d3cc5ffa58a6501dc5b9ffe2b330e64
Patrick Goulding <patrickgoulding@gmail.com>
2019-06-04 13:44:37 -0600

    add date select and method to format it

--- src/index.js
+++ src/index.js
@@ -22,23 +22,6 @@ fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1903/room-services/roomServ
   .then(remote => data.services = remote.roomServices)
   .catch(err => console.error(err))
 
-const todaysDate = () => {
-  let today = new Date();
-  let dd = today.getDate();
-  let mm = today.getMonth() + 1;
-  let yyyy = today.getFullYear();
-
-  if (dd < 10) {
-    dd = '0' + dd
-  }
-
-  if (mm < 10) {
-    mm = '0' + mm
-  }
-
-  let thisDay = dd + '/' + mm + '/' + yyyy;
-  return thisDay;
-}
 
 // An example of how you import jQuery into a JS file if you use jQuery in that file
 import $ from 'jquery';
@@ -61,16 +44,44 @@ import domUpdates from './domUpdates';
 import RoomServiceRepo from './RoomServiceRepo';
 import BookingRepo from './BookingsRepo';
 import Customer from './Customer';
+import RoomService from './RoomService';
 
-let userRepo;
-let main; 
-let orderRepo;
-let bookingRepo;
-let user;
 // let roomRepo;
 $(document).ready(function () {
-  
+  let userRepo;
+  let main; 
+  let orderRepo;
+  let bookingRepo;
+  let user;
+  let roomService;
+
+  const todaysDate = () => {
+    let today = new Date();
+    let dd = today.getDate();
+    let mm = today.getMonth() + 1;
+    let yyyy = today.getFullYear();
+
+    if (dd < 10) {
+      dd = '0' + dd
+    }
+
+    if (mm < 10) {
+      mm = '0' + mm
+    }
+
+    let thisDay = dd + '/' + mm + '/' + yyyy;
+    return thisDay;
+  }
+
   console.log(todaysDate())
+
+  setTimeout(() => {
+    main = new Main(data.bookings, data.services, data.rooms, '15/07/2019')
+    userRepo = new UserRepo(data.users)
+    orderRepo = new RoomServiceRepo(data.services, '15/07/2019')
+    bookingRepo = new BookingRepo(data.bookings)
+  }, 3000);
+
   $('ul.tabs li').click(function () {
     var tab_id = $(this).attr('data-tab');
 
@@ -81,33 +92,44 @@ $(document).ready(function () {
     $("#" + tab_id).addClass('current');
   })
   
-  setTimeout(() => {
-    main = new Main(data.bookings, data.services, data.rooms, '15/07/2019')
-    userRepo = new UserRepo(data.users)
-    orderRepo = new RoomServiceRepo(data.services, '15/07/2019')
-    bookingRepo = new BookingRepo(data.bookings)
-  }, 3000);
 
-  // const allUsers = new UserRepo(data.users)
-  // const allRooms = new RoomRepo(data.rooms)
-  // const allBookings = new BookingsRepo(data.bookings)
-  // const allServices = new RoomServiceRepo(data.roomservice)
+  $('#tab__customers-search').on('input', function () {
+    let typed = $('#tab__customers-search').val()
+    if (typed.length > 1) {
+      userRepo.showUsers(typed)
+      $('.tab__customers-output li').on('click', function () {
+        let customerID = $(this).attr('data-id')
+        let parsedID = parseInt(customerID)
+        console.log(parsedID)
+        user = new Customer(data.users, parsedID, undefined)
+        roomService = new RoomService(data.services, parsedID)
+        roomService.breakDown()
+        roomService.perDate(todaysDate)
+        roomService.forAllDays()
+      })
+    } else {
+      domUpdates.promptNewUser()
+    }
+  })
+
+  $('#orders__date-submit').on('click', function () {
+    const reorder = () => {
+      let dateSpec = $('#orders__date-select').val()
+      let splitDate = dateSpec.split('-')
+      let year = splitDate[0]
+      let month = splitDate[1]
+      let day = splitDate[2]
+      return `${day}/${month}/${year}`
+    }
+    // console.log(reorder())
+    orderRepo.totalServicesDate(reorder())
+  })
+
 })
-$('#tab__customers-search').on('input', function () {
-  let typed = $('#tab__customers-search').val()
-  if (typed.length > 1) {
-    userRepo.showUsers(typed)
-    $('.tab__customers-output li').on('click', function () {
-      let customerID = $(this).attr('data-id')
-      let parsedID = parseInt(customerID)
-      console.log(parsedID)
-      user = new Customer(data.users, parsedID, undefined)
-      
-    })
-  } else {
-    domUpdates.promptNewUser()
-  }
-})
+// const allUsers = new UserRepo(data.users)
+// const allRooms = new RoomRepo(data.rooms)
+// const allBookings = new BookingsRepo(data.bookings)
+// const allServices = new RoomServiceRepo(data.roomservice)
 
 // const showCustomerData = user => {
 //   const customer = new Customer(usersData, user)
