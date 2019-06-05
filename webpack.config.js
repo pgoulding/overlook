1a9fdd21f09757cde039ec6d27319f9f4fca5ec7
Patrick Goulding <patrickgoulding@gmail.com>
2019-06-05 04:26:48 -0600

    webpack to push up to gh pages

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,8 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'main.bundle.js'
+    filename: 'main.bundle.js',
+    publicPath: "/overlook/"
   },
   devtool: 'inline-source-map',
   mode: 'development',
